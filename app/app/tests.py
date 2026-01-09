16a77634a6e6478263ae4466added9bf29d578ce
Abbas <gitabbas26@gmail.com>
2026-01-09 19:51:16 +0400

    Added docker compose and checks for db

--- app/app/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import SimpleTestCase
-from . import calc
-
-class JustTest(SimpleTestCase):
-
-    def test_subtract_numbers(self):
-        res = calc.subtract(10, 15)
-
-        self.assertEqual(res, 5)
