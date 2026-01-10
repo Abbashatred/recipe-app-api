58d2dcc2306ee74ff7b9053c36d074883d5b3ab5
Abbas <gitabbas26@gmail.com>
2026-01-10 13:31:45 +0400

    Fixed admin panel problem

--- app/core/admin.py
+++ app/core/admin.py
@@ -15,7 +15,7 @@ class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
     fieldsets = (
-        (None, {'fields': {'email', 'password'}}),
+        (None, {'fields': ('email', 'password')}),
         (
             _('Permissions'),
             {
@@ -26,7 +26,7 @@ class UserAdmin(BaseUserAdmin):
                 )
             }
         ),
-        (_('Important dates'), {'fields':('last_login',)})
+        (_('Important dates'), {'fields': ('last_login',)})
     )
     readonly_fields = ['last_login']
 
